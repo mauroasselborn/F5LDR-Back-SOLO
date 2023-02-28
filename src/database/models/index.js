fe6bc8379ea88ed6f6ca435886715347e0c4a694
Mauro <mauasselborn@gmail.com>
2023-02-28 02:24:38 -0300

    Update

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -4,10 +4,10 @@ import Category from './Category.js'
 import Product_Sale from './Product_Sale.js'
 import Sale from './Sale.js'
 
-await Brand.sync({ alter: true })
-await Category.sync({ alter: true })
-await Sale.sync({ alter: true })
-await Product.sync({ alter: true })
-await Product_Sale.sync({ alter: true })
+// await Brand.sync({ alter: true })
+// await Category.sync({ alter: true })
+// await Sale.sync({ alter: true })
+// await Product.sync({ alter: true })
+// await Product_Sale.sync({ alter: true })
 
 export { Brand, Category, Product, Product_Sale, Sale }
